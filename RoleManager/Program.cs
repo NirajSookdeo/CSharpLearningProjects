58674b3db842bfe1721a3bd128a490d4f6c5dbf3
Niraj <sookdeo.n@outlook.com>
2026-01-20 14:41:24 -0500

    Day 20 Role Manager: completed v1.0 with working loop, most edge cases, roster display at the end, parallel lists

--- RoleManager/Program.cs
+++ RoleManager/Program.cs
@@ -1,52 +1,128 @@
 ﻿using System.Reflection.Metadata.Ecma335;
-bool validInput = false;
+bool gettingName = true;
+bool gettingRole = false;
+bool confirmingRole = false;
+bool addingAnother = false;
+bool exitProgram = false;
+
 string? employeeName = "";
 string? employeeRole = "";
 string? confirmation = "";
+int employeeNumber = 1;
+int guestCount = 0;
 
-Console.Write("Hello! Welcome to your company's Employee Role Manager!\n\nPlease enter the name of the first employee for this session:\n> ");
-employeeName = CheckForNull(Console.ReadLine(), "Please enter a name.");
-employeeName = char.ToUpper(employeeName[0]) + employeeName.Substring(1).ToLower();
+List<string> names = new List<string>();
+List<string> roles = new List<string>();
 
+Console.WriteLine("Hello! Welcome to your company's Employee Role Manager!");
 
-Console.Write($"What role will {employeeName} have on your team?\n- Administrator\n- User\n- Guest\n> ");
-employeeRole = Console.ReadLine().ToLower();
-
-
-while (!validInput)
+while (!exitProgram)
 {
-    switch (employeeRole)
+    if (gettingName)
     {
-        case "administrator":
-        case "user":
-        case "guest":
-            validInput = true;
-            break;
-        case "":
-            employeeRole = CheckForNull(employeeRole, "Please enter a role.\n- Administrator\n- User\n- Guest");
-            continue;
-        default:
-            Console.Write($"You entered {employeeRole}, the available roles are:\n- Administrator\n- User\n- Guest\n\nPlease enter one of these roles:\n> ");
-            employeeRole = Console.ReadLine().ToLower();
-            employeeRole = CheckForNull(employeeRole, "Please enter a role.\n- Administrator\n- User\n- Guest");
-            continue;
-    }
-    Console.Write($"Are you sure you want to grant employee {employeeName} the role of {employeeRole}? [Y/N]\n> ");
-    confirmation = Console.ReadLine().ToLower();
+        Console.Write($"Please enter Employee #{employeeNumber}'s name.\n> ");
+        employeeName = CheckForNull(Console.ReadLine(), "Please enter the name of your employee:");
+        employeeName = char.ToUpper(employeeName[0]) + employeeName.Substring(1).ToLower();
 
-    //PICK UP FROM HERE - N SHOULD LOOP BACK TO TOP ----------------------------------
-    while (confirmation != "y" && confirmation != "n")
+        gettingName = false;
+        gettingRole = true;
+    }
+    if (gettingRole)
     {
-        Console.Write($"You entered {confirmation}, type \"Y\" or \"N\" to accept or refuse assignment.\n> ");
-        confirmation = Console.ReadLine().ToLower();
+        Console.Write($"What role will {employeeName} have on your team?\n\n{RolesAndPermissions()}\n\n> ");
+        employeeRole = CheckForNull(Console.ReadLine().ToLower(), $"What role will {employeeName} have on your team?\n\n{RolesAndPermissions()}");
+
+        switch (employeeRole)
+        {
+            case "administrator":
+            case "user":
+            case "guest":
+                gettingRole = false;
+                confirmingRole = true;
+                break;
+            default:
+                Console.Write($"You entered {employeeRole}, which is not a valid role. ");
+                break;
+        }
+    }
+
+    if (confirmingRole)
+    {
+        Console.Write($"Are you sure you want to grant employee {employeeName} the role of {employeeRole}? [Y/N]\n> ");
+        confirmation = CheckForNull(Console.ReadLine().ToLower(), $"Please type \"Y\" or \"N\" to accept or refuse.");
+
+        while (confirmation != "y" && confirmation != "n")
+        {
+            Console.Write($"You entered {confirmation}, type \"Y\" or \"N\" to accept or refuse.\n> ");
+            confirmation = Console.ReadLine().ToLower();
+        }
 
         if (confirmation == "y")
         {
-            //code here
+            if (employeeRole == "guest")
+            {
+                if (guestCount >= 3)
+                {
+                    Console.WriteLine("You cannot add more guests, there is a MAX of 3.");
+                    confirmingRole = false;
+                    gettingRole = true;
+                    continue;
+                }
+                guestCount++;
+            }
+            confirmingRole = false;
+            addingAnother = true;
+            names.Add(employeeName);
+            roles.Add(employeeRole);
         }
         else
         {
-            //code here
+            confirmingRole = false;
+            gettingRole = true;
+            Console.WriteLine();
+        }
+    }
+
+    if (addingAnother)
+    {
+        Console.Write("Would you like to add another person to the team? [Y/N]\n> ");
+        confirmation = CheckForNull(Console.ReadLine().ToLower(), $"Please type \"Y\" or \"N\" to accept or refuse.");
+
+        while (confirmation != "y" && confirmation != "n")
+        {
+            Console.Write($"You entered {confirmation}, type \"Y\" or \"N\" to accept or refuse.\n> ");
+            confirmation = Console.ReadLine().ToLower();
+        }
+
+        if (confirmation == "y")
+        {
+            employeeNumber++;
+            addingAnother = false;
+            gettingName = true;
+        }
+        else
+        {
+            Console.WriteLine("Thank you for using your company's Employee Role Manager!");
+            addingAnother = false;
+            exitProgram = true;
+        }
+    }
+
+    
+}
+
+Console.WriteLine("\nYour Team Roster:\nName\t \tRole\n" +
+                  "---------------------------------------------");
+
+string[] roleOrder = { "administrator", "user", "guest" };
+
+foreach (string role in roleOrder)
+{
+    for (int i = 0; i < names.Count; i++)
+    {
+        if (roles[i] == role)
+        {
+            Console.WriteLine($"{names[i]}\t-\t{char.ToUpper(roles[i][0]) + roles[i].Substring(1).ToLower()}");
         }
     }
 }
@@ -62,4 +138,19 @@ string CheckForNull(string input, string repromptUser)
         correctLine = Console.ReadLine();
     }
     return correctLine;
+}
+
+string RolesAndPermissions()
+{
+    return $"+ Administrator\n" +
+                "\t- Create/Delete users\n" +
+                "\t- Modify all settings\n" +
+                "\t- CANNOT delete own account (requires 2 admins)\n" +
+                "\t- All actions will be recorded (Found in audit log)\n" +
+            $"+ User\n" +
+                "\t- Standard operations for own account\n" +
+                "\t- Can remove guests but cannot operate on members at higher level\n" +
+            $"+ Guest\n" +
+                "\t- Can ONLY view, no modifications allowed\n" +
+               $"\t- There is a limit of 3 guests per team (Current: {guestCount}/3)";
 }
